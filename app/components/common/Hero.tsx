7ea056fa9f2d28ae87d324bf65266ed9ed3ca182
Alvin Quach <alvinwquach@gmail.com>
2024-06-06 01:08:00 -0700

    Create hero and add to team page

---
+++ app/components/common/Hero.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Section from "./Section";
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+import cn from "clsx";
+
+type HeroProps = {
+  image: {
+    mobile?: StaticImageData;
+    desktop?: StaticImageData;
+  };
+  title: React.ReactNode;
+  description: React.ReactNode;
+  desktopClassName?: string;
+  imageClassName?: string;
+};
+
+function Hero({
+  image,
+  title,
+  description,
+  desktopClassName,
+  imageClassName,
+}: HeroProps) {
+  return (
+    <>
+      <div
+        className={cn(
+          "hidden lg:block h-[420px] w-full bg-contain bg-no-repeat bg-right-top bg-primary border-b border-zinc-700",
+          desktopClassName
+        )}
+        style={{
+          backgroundImage: image.desktop ? `url(${image.desktop.src})` : "",
+        }}
+      >
+        <Section
+          wrapperStyle={{
+            height: "100%",
+          }}
+          style={{
+            height: "100%",
+          }}
+        >
+          <div className="flex flex-col aligns-center justify-center h-full max-w-[720px]">
+            <h1 className="tracking-tight text-5xl font-title uppercase">
+              {title}
+            </h1>
+            <p className="mt-8 text-xl text-white font-light">{description}</p>
+          </div>
+        </Section>
+      </div>
+
+      <div className="block lg:hidden bg-black border-b border-b-zinc-700">
+        {image.mobile && (
+          <div className="h-[420px] w-full relative flex-1">
+            <Image
+              src={image.mobile}
+              className={cn("object-cover object-center", imageClassName)}
+              placeholder="blur"
+              alt="hero image"
+              fill={true}
+            />
+          </div>
+        )}
+
+        <div className="p-8">
+          <h2 className="text-3xl tracking-tight font-title uppercase text-center">
+            {title}
+          </h2>
+          <p className="mt-4 text-xl text-gray-400 font-light text-center">
+            {description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
--- app/team/[team]/page.tsx
+++ app/team/[team]/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import Section from "@/app/components/common/Section";
 import Image from "next/image";
 import { PortableText } from "next-sanity";
+import { rubik_scribble } from "@/util/fonts";
+import Section from "@/app/components/common/Section";
+import Hero from "@/app/components/common/Hero";
 import { GET_SPECIFIC_TEAM } from "@/graphql/queries";
 import { useSuspenseQuery } from "@apollo/client";
 import { PortableTextBlock } from "@portabletext/types";
+import MobileHero from "../../../public/images/joint-rehab-photo.jpg";
+import DesktopHero from "../../../public/images/joint-rehab-photo.jpg";
 
 interface TeamMember {
   name: string;
@@ -72,47 +76,80 @@ function Team({ params }: TeamProps) {
     teamData.sort((a, b) => order.indexOf(a.name) - order.indexOf(b.name));
   }
 
+  function formatTeamName(team: string): string {
+    return team.split("-").join(" ");
+  }
+
   return (
-    <Section>
-      <div>
-        {teamData.map((member) => (
-          <div
-            key={member.name}
-            className="bg-slate-800 flex flex-col md:flex-row pb-12 [&:not(:first-child)]:pt-12 [&:not(:last-child)]:border-b border-b-zinc-700"
-          >
-            <div className="relative flex justify-center w-64 h-64 md:w-48 md:h-48">
-              <Image
-                src={member.image?.asset?.url ?? ""}
-                alt={`Headshot of ${member.name}` ?? "Member image"}
-                className="rounded-full border-2 border-secondary  drop-shadow-md transform transition-all duration-300 hover:scale-105 overflow-hidden object-cover"
-                fill={true}
-              />
+    <>
+      <Hero
+        title={
+          <>
+            <div className="flex gap-x-4">
+              <h1
+                className={`${rubik_scribble.className} text-5xl sm:text-7xl text-white uppercase leading-normal sm:leading-none`}
+              >
+                {"Meet "}
+                <span>the</span>
+              </h1>
+
+              <strong className="text-5xl sm:text-7xl text-white uppercase leading-normal sm:leading-none font-semibold">
+                team
+              </strong>
             </div>
-            <div className="pt-8 md:pt-0 flex-1 flex flex-col pl-0 md:pl-16">
-              <h2 className="text-xl sm:text-2xl text-primary font-bold mb-1 text-center sm:text-left">
-                {member.name}
-              </h2>
-              <h3 className="text-gray-400 mb-4 text-center sm:text-left">
-                {member.jobTitle}
-              </h3>
-              <PortableText
-                value={member.descriptionRaw}
-                components={{
-                  block: {
-                    normal: ({ children }) => (
-                      <p className="mb-4 font-light text-gray-200">
-                        {children}
-                      </p>
-                    ),
-                  },
-                }}
-              />
+          </>
+        }
+        description={`Get to know the people that create our ${formatTeamName(params.team)} team.`}
+        desktopClassName="bg-contain"
+        imageClassName="!object-cover !object-top"
+        image={{
+          mobile: MobileHero,
+          desktop: DesktopHero,
+        }}
+      />
+
+      <Section>
+        <div>
+          {teamData.map((member) => (
+            <div
+              key={member.name}
+              className="bg-slate-800 flex flex-col md:flex-row pb-12 [&:not(:first-child)]:pt-12 [&:not(:last-child)]:border-b border-b-zinc-700"
+            >
+              <div className="relative flex justify-center w-64 h-64 md:w-48 md:h-48">
+                <Image
+                  src={member.image?.asset?.url ?? ""}
+                  alt={`Headshot of ${member.name}` ?? "Member image"}
+                  className="rounded-full border-2 border-secondary  drop-shadow-md transform transition-all duration-300 hover:scale-105 overflow-hidden object-cover"
+                  fill={true}
+                />
+              </div>
+              <div className="pt-8 md:pt-0 flex-1 flex flex-col pl-0 md:pl-16">
+                <h2 className="text-xl sm:text-2xl text-primary font-bold mb-1 text-center sm:text-left">
+                  {member.name}
+                </h2>
+                <h3 className="text-gray-400 mb-4 text-center sm:text-left">
+                  {member.jobTitle}
+                </h3>
+                <PortableText
+                  value={member.descriptionRaw}
+                  components={{
+                    block: {
+                      normal: ({ children }) => (
+                        <p className="mb-4 font-light text-gray-200">
+                          {children}
+                        </p>
+                      ),
+                    },
+                  }}
+                />
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </Section>
+          ))}
+        </div>
+      </Section>
+    </>
   );
 }
+
 
 export default Team;
