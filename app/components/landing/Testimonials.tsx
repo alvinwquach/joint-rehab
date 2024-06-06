979b4debf4dda11de560cd3a5c6344b5938bcfff
Alvin Quach <alvinwquach@gmail.com>
2024-06-06 16:00:11 -0700

    Create testimonials

---
+++ app/components/landing/Testimonials.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import Image from "next/image";
+import cn from "clsx";
+import { TestimonialFragment } from "@/src/gql/graphql";
+
+interface TestimonialCardProps {
+  name: string | null | undefined;
+  testimonial: string | null | undefined;
+  imageUrl: string | null | undefined;
+}
+
+const TestimonialCard = ({
+  name,
+  testimonial,
+  imageUrl,
+}: TestimonialCardProps) => {
+  return (
+    <figure className="group inline-flex gap-4 p-6 mb-4 w-full relative flex-col-reverse bg-primary highlight-white/5 rounded-lg">
+      <blockquote>
+        <p className="text-gray-400">{testimonial}</p>
+      </blockquote>
+
+      <figcaption className="flex items-center space-x-4">
+        <Image
+          src={imageUrl ?? ""}
+          alt={`${name}'s profile picture`}
+          className="flex-none rounded-full object-cover drop-shadow-md"
+          loading="lazy"
+          width={56}
+          height={56}
+        />
+        <div className="flex-auto">
+          <div className="text-base text-slate-50 font-semibold">
+            <div>{name}</div>
+          </div>
+        </div>
+      </figcaption>
+    </figure>
+  );
+};
+
+interface TestimonialsProps {
+  hasShowMore?: boolean;
+  testimonials: TestimonialFragment[];
+}
+
+const Testimonials = ({
+  hasShowMore = false,
+  testimonials,
+}: TestimonialsProps) => {
+  const [showMore, setShowMore] = React.useState(!hasShowMore);
+  return (
+    <>
+      <div className="relative">
+        <div className="flex pb-16 flex-col items-center justify-center">
+          <h2 className="text-3xl sm:text-4xl text-center font-title uppercase">
+            Hear from our <strong>clients</strong>
+          </h2>
+        </div>
+        <div
+          className={cn({
+            ["max-h-[27rem]"]: !showMore,
+            ["overflow-hidden"]: !showMore,
+          })}
+        >
+          <div className="columns-1 gap-x-4 md:columns-2 lg:columns-3">
+            {testimonials?.map((testimonial, index) => (
+              <TestimonialCard
+                key={index}
+                name={testimonial.name}
+                testimonial={testimonial.testimonial}
+                imageUrl={testimonial.image?.asset?.url ?? ""}
+              />
+            ))}
+          </div>
+          {hasShowMore && (
+            <div
+              className={cn("inset-x-0 flex justify-center absolute", {
+                ["pt-32"]: !showMore,
+                ["bg-gradient-to-t bottom-0 pb-0 pointer-events-none from-black"]:
+                  !showMore,
+              })}
+            >
+              {!showMore && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    setShowMore(!showMore);
+                  }}
+                  className="relative focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 text-sm text-white font-semibold h-12 px-6 rounded-lg flex items-center bg-sky-700 hover:bg-sky-500 pointer-events-auto"
+                >
+                  Show More...
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Testimonials;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,38 @@
-import Image from "next/image";
+"use client";
+
+import { useQuery, useSuspenseQuery } from "@apollo/client";
 import Section from "./components/common/Section";
 import WelcomeToJointRehab from "./components/landing/WelcomeToJointRehab";
 import AboutUs from "./components/landing/AboutUs";
 import Values from "./components/landing/Values";
+import { Testimonial } from "@/types/Testimonial";
+import { GET_TESTIMONIALS } from "@/graphql/queries";
+import Testimonials from "./components/landing/Testimonials";
+
+interface TestimonialsQueryResult {
+  allTestimonial: Testimonial[];
+}
 
 export default function Home() {
+  const { data: testimonialData } =
+    useSuspenseQuery<TestimonialsQueryResult>(GET_TESTIMONIALS);
+
+  const testimonials = testimonialData?.allTestimonial ?? [];
+  console.log(testimonials);
+
   return (
     <main>
       <Section className={Section.Colors.Dark}>
         <WelcomeToJointRehab />
       </Section>
-      <Section bg={Section.Colors.Light} className="!py-16">
+      <Section className={Section.Colors.Light}>
         <AboutUs />
       </Section>
-
-      <Section bg={Section.Colors.Light} className="!py-16">
+      <Section className={Section.Colors.Light}>
         <Values />
+      </Section>
+      <Section>
+        <Testimonials hasShowMore={true} testimonials={testimonials} />
       </Section>
     </main>
   );
