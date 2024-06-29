7927ac7bc4158f64f279fa1f7334e340fa814310
Alvin Quach <alvinwquach@gmail.com>
2024-06-29 10:12:25 -0700

    Create request appointment button on service page

--- app/services/[service]/page.tsx
+++ app/services/[service]/page.tsx
@@ -4,6 +4,7 @@ import Section from "@/app/components/common/Section";
 import { GET_SPECIFIC_SERVICE } from "@/graphql/queries";
 import { getClient } from "@/app/lib/apollo-client";
 import { formatTeamAndServiceName } from "@/util/formatTeamAndServiceName";
+import Link from "next/link";
 
 interface ServiceProps {
   params: {
@@ -39,11 +40,11 @@ async function Service({ params }: ServiceProps) {
     <Section>
       <div className="flex flex-col md:flex-row pb-12 md:pb-0">
         <div className="w-full flex justify-center">
-          <div className="w-2/3 ">
+          <div className="w-2/3">
             <Image
               src={service.image.asset.url}
               alt={`Image of ${service.name}`}
-              className="object-cover rounded-lg drop-shadow-md transform transition-all duration-300 "
+              className="object-cover rounded-lg drop-shadow-md transform transition-all duration-300"
               width={1200}
               height={600}
               priority
@@ -51,21 +52,29 @@ async function Service({ params }: ServiceProps) {
           </div>
         </div>
       </div>
-      <div className="flex-1 flex flex-col justify-center ">
-        <h2 className="text-3xl sm:text-4xl text-primary font-bold my-4">
-          {service.name}
-        </h2>
+      <div className="flex-1 flex flex-col justify-center">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center my-4">
+          <h2 className="text-3xl sm:text-4xl text-primary font-bold">
+            {service.name}
+          </h2>
+          <Link
+            href="/request-appointment"
+            className="mt-4 md:mt-0 bg-primary text-white font-bold py-2 px-4 rounded hover:bg-primary-dark transition-all duration-300"
+          >
+            Request Appointment
+          </Link>
+        </div>
+        <PortableText
+          value={service.descriptionRaw}
+          components={{
+            block: {
+              normal: ({ children }) => (
+                <p className="mb-4 font-light text-gray-200">{children}</p>
+              ),
+            },
+          }}
+        />
       </div>
-      <PortableText
-        value={service.descriptionRaw}
-        components={{
-          block: {
-            normal: ({ children }) => (
-              <p className="mb-4 font-light text-gray-200">{children}</p>
-            ),
-          },
-        }}
-      />
     </Section>
   );
 }
