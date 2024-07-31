37d5873d21b9ca69f4afe745e7245ee4808b3742
Alvin Quach <alvinwquach@gmail.com>
2024-07-31 15:27:07 -0700

    Update request appointment styling

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -35,7 +35,7 @@ function Footer({
   };
 
   return (
-    <footer className="bg-current bg-fourth text-white py-12">
+    <footer className="bg-current bg-fourth  text-white py-12">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Markham Plaza Location */}
