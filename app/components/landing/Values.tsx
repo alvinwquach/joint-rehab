c623ca325e89b7fb3b83a39059b3f752b93e68db
Alvin Quach <alvinwquach@gmail.com>
2024-07-31 22:06:17 -0700

    Update styling

--- app/components/landing/Values.tsx
+++ app/components/landing/Values.tsx
@@ -13,7 +13,7 @@ function Values() {
   return (
     <div className="w-full flex ">
       <div className="max-w-screen-xl m-auto w-10/12 flex flex-col items-center gap-20">
-        <h2 className="capitalize text-white text-4xl sm:text-5xl font-roboto font-bold text-center">
+        <h2 className="capitalize text-fifth text-4xl sm:text-5xl font-roboto font-bold text-center">
           Our Values
         </h2>
         <div className="flex flex-col w-full justify-between md:flex-row md:max-w-3xl max-w-xs gap-6 text-white">
